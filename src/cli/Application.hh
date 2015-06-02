f52577e0db9d2aad9476abf89c1a58c3a10123da
holyshared <holy.shared.design@gmail.com>
2015-06-02 13:45:02 +0900

    add application

---
+++ src/cli/Application.hh
@@ -0,0 +1,54 @@
+<?hh //strict
+
+namespace specify\cli;
+
+use specify\Specify;
+use specify\DefaultLifeCycleNotifier;
+use specify\example\ExamplePackage;
+use specify\result\ExamplePackageResult;
+use specify\collector\ExampleGroupCollector;
+
+
+class Application
+{
+
+    public function run(Argument $argv) : void
+    {
+        $configFile = $argv->getConfigFile();
+        $loadConfigFile = getcwd() . '/' . $configFile;
+
+        include_once $loadConfigFile;
+
+        $config = Specify::currentConfig();
+
+        $lifeCycleNotifier = new DefaultLifeCycleNotifier(ImmVector {
+            $config->getExampleReporter()
+        });
+
+        $collector = new ExampleGroupCollector();
+        $groups = $collector->collectFrom($config->getPackage());
+
+        $shuffleGroup = Vector {};
+
+        foreach ($groups as $group) {
+            $shuffleGroup->add($group);
+        }
+        $shuffleGroup->shuffle();
+
+        $package = new ExamplePackage('', $shuffleGroup->toImmVector());
+        $packageResult = $package->verify($lifeCycleNotifier);
+
+        $this->shutdown($packageResult);
+    }
+
+    private function shutdown(ExamplePackageResult $result) : void
+    {
+        $success = 0;
+
+        if ($result->isFailed()) {
+            exit($success);
+        }
+        exit(1);
+    }
+
+}
