401f39751955b20ca74937347f9fee107a15fd50
holyshared <holy.shared.design@gmail.com>
2015-06-03 13:40:06 +0900

    Add SpecificationFileCollector spec

---
+++ specs/fixtures/specs/Plain.hh
@@ -0,0 +1,16 @@
+<?hh //strict
+
+namespace specify\fixtures;
+
+/**
+ * This file is not subject to collection
+ */
+class Plain
+{
+
+    <<Example("example1")>>
+    public function example1() : void
+    {
+    }
+
+}
