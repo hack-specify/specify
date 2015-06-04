97b0a737a9ce71447b360bd3e824a297413bb765
holyshared <holy.shared.design@gmail.com>
2015-06-04 11:49:41 +0900

    add spec

--- specs/example/ExampleSpec.hh
+++ specs/example/ExampleSpec.hh
@@ -23,6 +23,23 @@ describe(Example::class, function() {
 
                 expect($result->getDescription())->toBe("example1");
                 expect($result->isPassed())->toBeTrue();
+                expect($result->isFailed())->toBeFalse();
+            });
+        });
+        context('when failed', function() {
+            beforeEach(function() {
+                $this->notifier = new NotifyRecorder();
+            });
+            it('returns passed result', function() {
+                $target = new FixtureExample();
+                $exampleMethod = new ReflectionMethod($target, 'example3');
+
+                $example = new Example($target, $exampleMethod);
+                $result = $example->verify($this->notifier);
+
+                expect($result->getDescription())->toBe("example3");
+                expect($result->isPassed())->toBeFalse();
+                expect($result->isFailed())->toBeTrue();
             });
         });
     });
